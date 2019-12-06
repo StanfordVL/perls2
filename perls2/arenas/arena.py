6ec263526d4fedc82cf8c1f038b449b6e341d4a1
Rohun Kulkarni <rohun@stanford.edu>
2019-12-06 11:04:31 -0800

    arena is pep8 compliant

--- perls2/arenas/arena.py
+++ perls2/arenas/arena.py
@@ -4,22 +4,21 @@ import numpy as np
 import yaml
 
 
-
 class Arena:
     """The class definition for arenas
-    Arenas contain interfaces for robots, sensors and objects. 
+    Arenas contain interfaces for robots, sensors and objects.
     """
-    
+
     def __init__(self,
-                config):
+                 config):
         """ Initialization function.
-            
-            Gets parameters from configuration file for managing experimental 
-            setup and episodes. These include randomized parameters for 
-            camera extrinsic/intrinsics, object placement. 
+
+            Gets parameters from configuration file for managing experimental
+            setup and episodes. These include randomized parameters for
+            camera extrinsic/intrinsics, object placement.
         Args:
             config (dict): A dict with config parameters
-        
+
         Returns:
             None
         """
@@ -29,28 +28,29 @@ class Arena:
 
         # Load camera parameters
         camera_extrinsics_cfg = self.config['sensor']['camera']['extrinsics']
-        
+
         self.camera_eye_pos = camera_extrinsics_cfg['eye_position']
         self.camera_target_pos = camera_extrinsics_cfg['target_position']
         self.camera_up_vector = camera_extrinsics_cfg['up_vector']
 
-        #Image parameters
+        # Image parameters
         camera_intrinsics_cfg = self.config['sensor']['camera']['intrinsics']
-        
+
         self.image_height = camera_intrinsics_cfg['image_height']
         self.image_width = camera_intrinsics_cfg['image_width']
         self.near_plane = camera_intrinsics_cfg['near_plane']
         self.far_plane = camera_intrinsics_cfg['far_plane']
-        self.fov = camera_intrinsics_cfg['fov'] 
+        self.fov = camera_intrinsics_cfg['fov']
 
-        self._rand_camera_intrin_cfg = self.config['sensor']['camera']['random']['intrinsics']
-        self._rand_camera_extrin_cfg = self.config['sensor']['camera']['random']['extrinsics']
+        self._rand_camera_intrin_cfg = (
+            self.config['sensor']['camera']['random']['intrinsics'])
+        self._rand_camera_extrin_cfg = (
+            self.config['sensor']['camera']['random']['extrinsics'])
         self._random_obj_cfg = self.config['object']['random']
-
 
     def random_vec_bounded(self, lower_bound, upper_bound):
         """Create a xf random position within bounds
-            Args: 
+            Args:
                 lower_bound (ndarray): lower limit for random position x,y,z
                 upper_bound (ndarray): upper limitfor random position x,y,z
             Returns:
@@ -59,16 +59,17 @@ class Arena:
             Note:  DOES NOT CHECK if upper bounds > lower bounds
         """
         size = np.size(upper_bound)
-        return np.random.random((size,))*(upper_bound - lower_bound) + lower_bound
+        return (np.random.random((size,))*(upper_bound - lower_bound) +
+                lower_bound)
 
     def randomize_param(self, config_key):
         """ Randomize parameter according to bounds found in config file
-            
+
             Args:
-                config_key (dict): config dict to random parameter definitions. 
+                config_key (dict): config dict to random parameter definitions.
                 -
             Returns:
-                -randomized Xf within the bounds of the upper and lower 
+                -randomized Xf within the bounds of the upper and lower
                 limits specified in the config file
             Example
 
@@ -77,8 +78,9 @@ class Arena:
 
                 cameraEyePosition = self.randomize_param(
                     self.config['sensor']['camera']['random']['intrinsics'])
-            
-            NOTE: bounds must be defined in config file as keys 'upper' and 'lower'
+
+            NOTE: bounds must be defined in config file as keys 'upper' and
+                'lower'
             NOTE: currently no checks on whether upper > lower TODO
             NOTE: does not check if string does not exist.
             NOTE: Does not check that dimensions of upper = lower
@@ -91,24 +93,15 @@ class Arena:
 
         return self.random_vec_bounded(lower_bound, upper_bound)
 
-
     def randomize_obj_pos(self):
         """Randomize camera placement in sim
             Args: None
-            Returns: 
+            Returns:
                 -camera_eye_pos (3f) randomized position
         """
 
         return self.randomize_param(self._random_obj_cfg['position'])
 
-
-    
     @property
     def view_matrix(self):
         raise NotImplementedError
-                     
-
-
-    
-
-    
