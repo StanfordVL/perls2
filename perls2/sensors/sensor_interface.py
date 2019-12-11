6e05aa0cc5b2e723f71da8d0bf4dc889a6743ec1
rohun-kulkarni <ctmk2020@gmail.com>
2019-12-11 15:16:57 -0800

    sensor interface pep8 compliant

--- perls2/sensors/sensor_interface.py
+++ perls2/sensors/sensor_interface.py
@@ -1,24 +1,24 @@
 """Abstract class for Sensor Interface
 """
 
-import abc # For abstract class definitions
-import six # For abstract class definitions
+import abc  # For abstract class definitions
+import six  # For abstract class definitions
+
 
 @six.add_metaclass(abc.ABCMeta)
 class SensorInterface(object):
     """Abstract interface to be implemented for a sensor
     """
 
-    def __init__(self, 
+    def __init__(self,
                  name):
         """
         Initialize variables
-        
+
         Parameters
         ----------
             name: name of sensor
         """
-
 
     def reset(self):
         """ Reset sensor
