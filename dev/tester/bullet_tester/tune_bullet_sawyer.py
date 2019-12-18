0db0218ff18330521706be7359c84dbf93e623c2
Rohun Kulkarni <rohunk@capri24.stanford.edu>
2019-12-17 20:01:52 -0800

    pushing for remote work. not functional

--- dev/tester/bullet_tester/tune_bullet_sawyer.py
+++ dev/tester/bullet_tester/tune_bullet_sawyer.py
@@ -6,8 +6,7 @@ from perls2.utils.yaml_config import YamlConfig
 import os
 import numpy as np
 import time
-
-
+from scipy.spatial.transform import Rotation as R
 #################### SETUP ################################
 # TODO: Change this to its own file in the tester folder
 # Create a pybullet simulation in isolation
@@ -132,61 +131,75 @@ def step_till_close_enough_fn(
 bullet_sawyer.set_joints_to_neutral_positions()
 print(np.shape(bullet_sawyer.jacobian))
 print("reset")
+print(bullet_sawyer._dof)
 # #target_joint_positions = [0] * 14
-print(pybullet.getMatrixFromQuaternion([0, 0, 0, 1]))
-target_joint_positions = (np.asarray([0,
-  -1.18,
-  0.00,
-  2.18,
-  0.00,
-  0.57,
-  3.3161, 0, 0,
-]) +
- np.asarray([-0.1]*9))
+
 #bullet_sawyer.set_joints_uncompensated(
 #    target_joint_positions)
 tuning_data = []
 tuning_torques = []
 # Add a disturbance to current position
 xd_pos = (np.array(bullet_sawyer.ee_pose[:3]) +
-    np.array([0.0, 0.10, 0.00]))
+    np.array([0.1, 0.0, 0.00]))
+
+# To add a disturbance to orientation, convert to
+# axis angle representation.
+curr_orn = R.from_quat(np.asarray(bullet_sawyer.ee_orientation))
+xd_orn = curr_orn
+# print(curr_orn.as_quat())
+# rot_disp = R.from_euler('z', 0.1, degrees=False)
+# xd_orn = R.from_matrix(np.dot(curr_orn.as_matrix(), rot_disp.as_matrix()))
+# orn_error = bullet_sawyer.controller.calculate_orientation_error(
+#     np.matrix.flatten(xd_orn.as_matrix()),
+#     np.matrix.flatten(curr_orn.as_matrix()))
+# print(orn_error)
+#xd_orn = R.from_rotvec(xd_orn_axis_angle + np.array([0, 0, 0.0]))
 
 # Maintain initial orientation
 xd =  np.hstack(
     (xd_pos,
-     np.asarray(bullet_sawyer.ee_orientation)))
-
-
-print(bullet_sawyer.N_q)
+     xd_orn.as_quat()))
+print("mass matrix")
+print(bullet_sawyer.mass_matrix)
+print("lambda x")
+print(bullet_sawyer.op_space_mass_matrix[0])
+print("lambda r")
+print(bullet_sawyer.op_space_mass_matrix[1])
+print("lambda")
+print(bullet_sawyer.op_space_mass_matrix[2])
+# print(bullet_sawyer.N_q)
 
 import matplotlib.pyplot as plt
 
 steps=0
 pose_step = []
+orn_list = []
+orn_aa_list = []
 kpkv_forces_list = []
 mass_kpkv_list = []
 osc_torques_list = []
 comp_torques_list = []
 grav_list = []
 
-
-max_steps = 2000
+posture = np.asarray(bullet_sawyer.motor_joint_positions[:7])
+max_steps = 5000
 input ("start recording?")
 while (steps <max_steps):
     # if(np.allclose(xd[0:3], bullet_sawyer.ee_pose[:3], 1e-2, 1e-2)):
     #     print("goal acheived")
     #     print("steps " +str(steps))
     #     break
-    kpkv_forces, mass_kpkv_forces, osc_torques, comp_torques = bullet_sawyer.compute_osc_compensated(xd)
+    orn_error, error, kpkv_forces, mass_kpkv_forces, osc_torques, comp_torques = (
+        bullet_sawyer.controller.compute_torques_compensated(
+            bullet_sawyer.state_dict, posture, xd))
     set_torques = bullet_sawyer.set_torques(comp_torques)
     pybullet.stepSimulation()
 
     # Save all the values at each step
     grav_list.append(bullet_sawyer.N_q)
-    orn_eul = np.asarray(pybullet.getEulerFromQuaternion(bullet_sawyer.ee_pose[3:]))
-    pose = np.hstack((np.asarray(bullet_sawyer.ee_pose[:3]),orn_eul))
-    pose_step.append(pose)
-
+    pose_step.append(error)
+    orn_list.append(orn_error[0])
+    orn_aa_list.append(orn_error[1])
     kpkv_forces_list.append(kpkv_forces)
     mass_kpkv_list.append(mass_kpkv_forces)
     osc_torques_list.append(osc_torques)
@@ -202,13 +215,20 @@ while (steps <max_steps):
 # Data is in format for each joint/dof at a time step [q1, q2, q3 ...]
 # we want the data for each joint/dof over time. q1[t=0, t=1, t=...]
 pose_error =[]
+orn_error_all = []
+orn_error_aa_all = []
 kpkv_forces_all = []
 mass_kpkv_forces_all = []
 osc_torques_all = []
 comp_torques_all =[]
 grav_all = []
+orn_error_all = np.transpose(orn_list)
+orn_error_aa_all = np.transpose(orn_aa_list)
+# for index in range(3):
+#     orn_error_all[index].append([step[index] for step in orn_list])
+#     orn_error_aa_all[index].append([step[index] for step in orn_aa_list])
 for index in range(6):
-    pose_error.append([(step[index] - xd[index]) for step in pose_step])
+    pose_error.append([step[index] for step in pose_step])
     kpkv_forces_all.append([step[index] for step in kpkv_forces_list])
     mass_kpkv_forces_all.append([step[index] for step in mass_kpkv_list])
 for index in range(7):
@@ -216,7 +236,29 @@ for index in range(7):
     osc_torques_all.append([step[index] for step in osc_torques_list])
     comp_torques_all.append([step_torque[index] for step_torque in comp_torques_list])
 
-###################### PLOTTING ##########################################
+# # PLOT ORIENTATION ERROR
+# for graph_num in range(3):
+#     plt.subplot(2,3,1+ graph_num)
+#     plt.xlabel('num_steps')
+
+#     plt.ylabel('orientation error (rad)')
+
+#     plt.title("orn error cross product")
+#     plt.plot(
+#         range(steps),
+#         orn_error_all[graph_num],'b')
+# for graph_num in range(3):
+#     plt.subplot(2,3,4+ graph_num)
+#     plt.xlabel('num_steps')
+
+#     plt.ylabel('orientation error (rad)')
+
+#     plt.title("orn error axis angle")
+#     plt.plot(
+#         range(steps),
+#         orn_error_aa_all[graph_num],'b')
+
+# ###################### PLOTTING ##########################################
 # Position error
 pose_error_titles = ['X error', 'Y Error', 'Z Error', 'Alpha Error', 'Beta Error', 'Gamma Error']
 kpkv_titles = ['(kpkv force x)','(kpkv force y)', '(kpkv force z)', '(kpkv torque alpha)', '(kpkv torque beta)', '(kpkv torque gamma)']
