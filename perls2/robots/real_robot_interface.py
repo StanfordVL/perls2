4f1c927f155757276aa0f4d32459f65f82e05571
Rohun Kulkarni <rohun@stanford.edu>
2019-12-20 15:27:48 -0500

    code clean up

--- perls2/robots/real_robot_interface.py
+++ perls2/robots/real_robot_interface.py
@@ -4,11 +4,13 @@ Author: Roberto Martin-Martin
         Rohun Kulkarni
 """
 
-import abc  # For abstract class definitions
-import six  # For abstract class definitions
-import redis # For communicating with RobotCtrlInterfaces 
+import abc   # For abstract class definitions
+import six   # For abstract class definitions
+import redis  # For communicating with RobotCtrlInterfaces
 
 from perls2.robots.robot_interface import RobotInterface
+
+
 class RealRobotInterface(RobotInterface):
     """Abstract interface to be implemented for each real and simulated
     robot.
@@ -20,14 +22,13 @@ class RealRobotInterface(RobotInterface):
                  controlType=None):
 
         self.config = config
-        
 
     def create(config, physics_id, arm_id):
         """Factory for creating robot interfaces based on config type
         """
         if (config['robot']['type'] == 'sawyer'):
             from perlsv2.interfaces.robots.real_sawyer_interface import RealSawyerInterface
-            return RealSawyerInterface(config=config, physics_id = physics_id, arm_id = arm_id)
-        else: 
-            print("invalid robot interface type") 
-
+            return RealSawyerInterface(
+                config=config, physics_id=physics_id, arm_id=arm_id)
+        else:
+            raise ValueError("invalid robot interface type. choose 'sawyer'")
