8ab992b55956ec766e4c91988c4f7ed81b7e8283
Rohun Kulkarni <rohun@stanford.edu>
2019-12-20 15:25:08 -0500

    code clean up

--- perls2/robots/real_panda_interface.py
+++ perls2/robots/real_panda_interface.py
@@ -7,10 +7,9 @@ Author: Roberto Martin-Martin
 import abc  # For abstract class definitions
 import six  # For abstract class definitions
 
-
-@six.add_metaclass(abc.ABCMeta)
-
 from perls2.robots.real_robot_interface import RealRobotInterface
+
+
 class RealPandaInterface(RealRobotInterface):
     """Abstract interface to be implemented for each real and simulated
     robot.
