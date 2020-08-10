0edcd3456c8b6abdd61970fe648818f7da077e1f
rohun <rohun@stanford.edu>
2020-08-10 13:28:56 -0700

    changed message type for joint pub rate publisher from Int32 to correct UInt16

--- perls2/ros_interfaces/sawyer_joint_pub_rate.py
+++ perls2/ros_interfaces/sawyer_joint_pub_rate.py
@@ -9,11 +9,11 @@ This script expects the sawyer to be on and running. The channel
 """
 
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import UInt16
 import argparse
 
 parser = argparse.ArgumentParser(description="Rate to publish joint state from Sawyer")
-parser.add_argument('--publish_rate', metavar='f', type=int, default=800, help="rate at which sawyer should publish joint state")
+parser.add_argument('--publish_rate', metavar='f', type=int, default=500, help="rate at which sawyer should publish joint state")
 args = parser.parse_args()
 
 
@@ -21,7 +21,7 @@ JOINT_STATE_PUBLISH_RATE = args.publish_rate
 
 
 rospy.logdebug("Creating joint state publish rate publisher")
-pub = rospy.Publisher('/robot/joint_state_publish_rate', Int32)
+pub = rospy.Publisher('/robot/joint_state_publish_rate', UInt16)
 rospy.init_node("sawyer_joint_pub_rate", anonymous=True)
 rate = rospy.Rate(10)
 
