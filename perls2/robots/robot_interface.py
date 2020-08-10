e6108736de1b3bd6af2d0f703a8a1d29066259ce
rohun <rohun@stanford.edu>
2020-08-10 13:30:54 -0700

    removed debug code for robot interface.

--- perls2/robots/robot_interface.py
+++ perls2/robots/robot_interface.py
@@ -179,7 +179,6 @@ class RobotInterface(object):
                 print("ACTION NOT SET")
 
     def set_controller_goal(self, **kwargs):
-    import  pdb; pdb.set_trace()
         self.controller.set_goal(**kwargs)
         self.action_set = True
 
@@ -214,7 +213,6 @@ class RobotInterface(object):
             if len(set_pos) != 3:
                 raise ValueError('set_pos incorrect dimensions, should be length 3')
 
-        import pdb; pdb.set_trace()
         self.check_controller("EEPosture")
 
         kwargs = {'delta': delta, 'set_pos': set_pos, 'set_ori':set_ori}
