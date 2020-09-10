5613d572ca186de6e0058f2a001bb397eba8990e
rohun <rohun-kulkarni@github.com>
2020-09-09 18:40:58 -0700

    Sawyer_urdf generated from intera added. Use this for opspace

---
+++ data/robot/rethink/sawyer_description/urdf/intera_tools_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/ros_ws/src/intera_common/intera_tools_description
+# Build directory: /home/robot/ros_ws/rtos_computer/intera_common/intera_tools_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
