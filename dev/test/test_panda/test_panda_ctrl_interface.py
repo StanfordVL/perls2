50cba32df4c7f7a0af3f9066bfef7afb8f59ad4b
rohun-kulkarni <rohun@stanford.edu>
2020-11-12 14:45:12 -0800

    removed fake franka_redis driver. to be used in separate tests

--- dev/test/test_panda/test_panda_ctrl_interface.py
+++ dev/test/test_panda/test_panda_ctrl_interface.py
@@ -11,43 +11,6 @@ from perls2.ros_interfaces.redis_values import *
 from perls2.ros_interfaces.panda_ctrl_interface import PandaCtrlInterface
 P = PandaKeys('cfg/franka-panda.yaml')
 
-# 1. Fake franka-panda redis driver set up.
-class FakeFrankaPanda(object):
-    """Class for faking panda redis driver.
-
-    Sets fake values for redis, mocking franka-panda.
-    """
-    def __init__(self):
-        self.driver_config = YamlConfig('cfg/franka-panda.yaml')
-        # some renaming for the panda redis interface.
-        redis_config = {"host": self.driver_config['redis']['ip'],
-                        "port": self.driver_config['redis']['port'],
-                        "driver_config":'cfg/franka-panda.yaml' }
-        self.redisClient = PandaRedisInterface(**redis_config)
-
-    def start(self):
-        self.redisClient.set(P.DRIVER_CONN_KEY, P.DRIVER_CONNECTED_VALUE)
-
-    def stop(self):
-        self.redisClient.set(P.DRIVER_CONN_KEY, P.DRIVER_DISCONN_VALUE)
-
-    def set_q(self, q):
-        """ Set fake joint positions in format franka-panda redis uses
-
-        Args:
-            q (list): 7f joint positions.
-        """
-        self.redisClient.set(P.ROBOT_STATE_Q_KEY, str(q))
-
-def test_fake_redis_driver():
-    redis_driver = FakeFrankaPanda()
-    # Start driver, check status changes.
-    redis_driver.start()
-    assert(redis_driver.redisClient.get(P.DRIVER_CONN_KEY) == b"running")
-    # Stop driver, check status changes.
-    redis_driver.stop()
-    assert(redis_driver.redisClient.get(P.DRIVER_CONN_KEY) == b"off")
-
 @pytest.fixture
 def panda_ctrl():
     panda = PandaCtrlInterface(config='cfg/panda_ctrl_config.yaml',
