298c3eafbe6af66512da50f43e98114cdf9d2d4d
test <test>
2020-11-16 11:56:55 -0800

    added full test for getting ee_pose from redis. checks for homogeneous matrix, rotation mat orthogonality, and ee_position values added to test_panda_redis.

--- dev/test/test_redis/test_panda_redis.py
+++ dev/test/test_redis/test_panda_redis.py
@@ -36,11 +36,39 @@ def test_get_driver_states(panda_redis, fake_driver):
 	driver_states = panda_redis.get_driver_states()
 	for key, state in driver_states.items():
 		if key == P.ROBOT_STATE_EE_POSE_KEY:
-			assert(driver_states[key].shape == (4,4))
+			ee_pose = driver_states[P.ROBOT_STATE_EE_POSE_KEY]
+			assert(ee_pose.shape == P.EE_POSE_SHAPE)
 			# make sure properly imported (column-major).
-			# last row should be 0, 0, 0, 1
-			assert np.all(driver_states[key][3, :] == [0, 0, 0, 1])
+			# last row should be 0, 0, 0, 1 (homogeneous transform)
+			assert np.all(ee_pose[3, :] == [0, 0, 0, 1])
+			# make sure rotation matrix is orthogonal.
+			rot_m =  ee_pose[0:3, 0:3]
+			print(np.dot(rot_m, rot_m.T))
+			assert np.all(np.isclose(np.dot(rot_m, rot_m.T), np.eye(3), rtol=1e-5, atol=1e-5))
+			# check values for ee_position (last column, 1st 3 rows.)
+			ee_pos = ee_pose[0:3, 3]
+			assert np.all(np.isclose(ee_pos,[0.534449340443935, 0.0364261218566121, 0.313053490964604]))
+
 
 		else:
 			assert(driver_states[key].shape == (7,))
+
+def test_get_driver_state_model(panda_redis, fake_driver):
+	driver_states = panda_redis.get_driver_states()
+	for key, state in driver_states.items():
+		if key == P.ROBOT_STATE_EE_POSE_KEY:
+			ee_pose = driver_states[P.ROBOT_STATE_EE_POSE_KEY]
+			assert(ee_pose.shape == P.EE_POSE_SHAPE)
+			# make sure properly imported (column-major).
+			# last row should be 0, 0, 0, 1 (homogeneous transform)
+			assert np.all(ee_pose[3, :] == [0, 0, 0, 1])
+			# make sure rotation matrix is orthogonal.
+			rot_m =  ee_pose[0:3, 0:3]
+			print(np.dot(rot_m, rot_m.T))
+			assert np.all(np.isclose(np.dot(rot_m, rot_m.T), np.eye(3), rtol=1e-5, atol=1e-5))
+			# check values for ee_position (last column, 1st 3 rows.)
+			ee_pos = ee_pose[0:3, 3]
+			assert np.all(np.isclose(ee_pos,[0.534449340443935, 0.0364261218566121, 0.313053490964604]))
+		else:
+			assert(driver_states[key].shape == (7,))	
 
