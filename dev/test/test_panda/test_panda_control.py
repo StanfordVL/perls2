477c52d63eea379c4d4f05c86b824b9d8f058322
test <test>
2020-11-16 16:26:33 -0800

    test script for holding ee_pose

--- dev/test/test_panda/test_panda_control.py
+++ dev/test/test_panda/test_panda_control.py
@@ -1,10 +1,25 @@
 """ Test script for real panda control
 """
 from perls2.ros_interfaces.panda_ctrl_interface import PandaCtrlInterface
+from dev.test.test_panda.fake_real_panda import FakePandaInterface
+from perls2.ros_interfaces.panda_redis_keys import PandaKeys
+import perls2.controllers.utils.transform_utils as T 
 import numpy as np
 
-# Test setting torques changes the control mode of the franka redis driver. 
-panda_ctrl = PandaCtrlInterface(config='cfg/panda_ctrl_config.yaml', controlType=None)
-panda_ctrl.set_torques(np.array([0.0001]*7))
+P = PandaKeys()
+
+real_panda = FakePandaInterface()
+real_panda.connect()
+
+# Get initial robot state from driver. 
+init_states = real_panda.redisClient.get_driver_states()
+init_ee_pose = init_states[P.ROBOT_STATE_EE_POSE_KEY]
+ee_pos, ee_ori_quat = T.mat2pose(init_ee_pose)
+
+
+real_panda.set_ee_pose(set_pos=ee_pos, set_ori=ee_ori_quat)
+print("set_pos {}".format(ee_pos))
+print("set_ori {}".format(ee_ori_quat))
+
 #panda_ctrl.set_to_float()
 
