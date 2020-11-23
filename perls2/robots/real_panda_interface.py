100752a25af26994baecc27bdb3fb299cc77387f
test <test>
2020-11-23 10:32:25 -0800

    real panda interface gets robot state from frankapanda redis keys

--- perls2/robots/real_panda_interface.py
+++ perls2/robots/real_panda_interface.py
@@ -19,6 +19,9 @@ from perls2.robots.real_robot_interface import RealRobotInterface
 from perls2.ros_interfaces.redis_interface import PandaRedisInterface
 from perls2.ros_interfaces.redis_keys import *
 from perls2.ros_interfaces.redis_values import * 
+import perls2.controllers.utils.transform_utils as T  
+from perls2.panda_redis_keys import PandaKeys
+P = PandaKeys()
 
 class RealPandaInterface(RealRobotInterface):
     """Abstract interface to be implemented for each real and simulated
@@ -35,6 +38,7 @@ class RealPandaInterface(RealRobotInterface):
         # Create redis interface specific to panda.
         redis_config = self.config['redis']
         self.redisClient = PandaRedisInterface(**self.config['redis'])
+        self._get_state_from_redis()
         self.RESET_TIMEOUT = 10
         self.set_controller_params_from_config()
 
@@ -54,3 +58,132 @@ class RealPandaInterface(RealRobotInterface):
             print("reset successful")
         else:
             print("reset failed")
+
+    def _get_state_from_redis(self):
+        driver_states = self.redisClient.get_driver_states()
+        self._ee_position, self._ee_orientation = T.mat2pose(driver_states[P.ROBOT_STATE_EE_POSE_KEY])
+        self._ee_pose = driver_states[P.ROBOT_STATE_EE_POSE_KEY]
+        self._ee_twist = T.calc_twist(driver_states[P.ROBOT_MODEL_JACOBIAN_KEY], driver_states[P.ROBOT_STATE_DQ_KEY])
+        self._ee_v = self._ee_twist[:3]
+        self._ee_w = self._ee_twist[3:]
+        self._q = driver_states[P.ROBOT_STATE_Q_KEY]
+        self._dq = driver_states[P.ROBOT_STATE_DQ_KEY]
+        self._tau = driver_states[P.ROBOT_STATE_TAU_KEY]
+        self._jacobian = driver_states[P.ROBOT_MODEL_JACOBIAN_KEY]
+        self._linear_jacobian = self._jacobian[:3, :]
+        self._angular_jacobian = self._jacobian[3:, :]
+        self._mass_matrix = driver_states[P.ROBOT_MODEL_MASS_MATRIX_KEY]
+        self._gravity = driver_states[P.ROBOT_MODEL_GRAVITY_KEY]
+
+    @property
+    def ee_position(self):
+        """list of three floats [x, y, z] of the position of the
+        end-effector.
+        """
+        return self._ee_position
+
+    @property
+    def ee_orientation(self):
+        """list of four floats [qx, qy, qz, qw] of the orientation
+        quaternion of the end-effector.
+        """
+        return self._ee_orientation
+
+    @property
+    def ee_pose(self):
+        """list of seven floats [x, y, z, qx, qy, qz, qw] of the 6D pose
+        of the end effector.
+        """
+        return self._ee_pose
+
+    @property
+    def ee_v(self):
+        """list of seven floats [x, y, z, qx, qy, qz, qw] of the 6D pose
+        of the end effector.
+        """
+        return self._ee_v
+
+    @property
+    def ee_w(self):
+        """list of seven floats [x, y, z, qx, qy, qz, qw] of the 6D pose
+        of the end effector.
+        """
+        return self._ee_w
+
+    @property
+    def ee_twist(self):
+        """list of seven floats [x, y, z, qx, qy, qz, qw] of the 6D pose
+        of the end effector.
+        """
+        return self._ee_twist
+
+    @property
+    def q(self):
+        """List of 7f describing joint positions (rad) of the robot arm.
+
+        Ordered from base to end_effector
+        """
+        return self._q
+
+    @property
+    def dq(self):
+        """List of 7f describing joint velocities (rad/s) of the robot arm.
+
+        Ordered from base to end_effector
+        """
+        return self._dq
+
+    @property
+    def tau(self):
+        """List of 7f describing joint torques (Nm)
+
+        Ordered from base to end_effector
+        """
+        return self._tau
+
+    @property
+    def jacobian(self):
+        """List of 7f describing joint velocities (rad/s) of the robot arm.
+
+        Ordered from base to end_effector
+        """
+        return self._jacobian
+
+    @property
+    def linear_jacobian(self):
+        """List of 7f describing joint velocities (rad/s) of the robot arm.
+
+        Ordered from base to end_effector
+        """
+        return self._linear_jacobian
+
+    @property
+    def angular_jacobian(self):
+        """List of 7f describing joint velocities (rad/s) of the robot arm.
+
+        Ordered from base to end_effector
+        """
+        return self._angular_jacobian
+
+    @property
+    def mass_matrix(self):
+        """List of 7f describing joint velocities (rad/s) of the robot arm.
+
+        Ordered from base to end_effector
+        """
+        return self._mass_matrix
+
+    def open_gripper(self):
+        """ Open robot gripper.
+        """
+        raise NotImplementedError
+
+    def close_gripper(self):
+        """ Close robot gripper
+        """
+        raise NotImplementedError
+
+    def set_gripper_to_value(self, value):
+        """ Set gripper to desired open/close value
+        """
+        raise NotImplementedError
