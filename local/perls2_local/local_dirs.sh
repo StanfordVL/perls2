8ef053c3fcfc660b69ce5cde37610a6375cea879
test <test>
2021-02-12 14:04:39 -0800

    Add local dir for setup
    Local directory added for real robot setup. This local directory contains configs and bash scripts specific to machine.
    This allows for redis configs, perls2 and driver config files to be created without being overwritten by changes to main perls2 repo.

---
+++ local/perls2_local/local_dirs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Source virtualenv environment with perls2 installed
+export SOURCE_ENV_CMD="source ~/p2env/bin/activate"
+# Directory for perls2 repo
+export PERLS2_DIR="$HOME/perls2"
+# Directory for franka-panda-iprl repo
+export DRIVER_DIR="$HOME/franka-panda-iprl"
+# Path to redis conf file.
+export REDIS_CONF="$HOME/goddard_redis.conf"
