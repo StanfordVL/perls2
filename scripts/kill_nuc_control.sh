cb0c537e6b59df6be1a630ab2e743782edd41a58
test <test>
2021-02-12 16:16:58 -0800

    make kill nuc ctrl general
    Add arg for session name to kill nuc_ctrl bash script

--- scripts/kill_nuc_control.sh
+++ scripts/kill_nuc_control.sh
@@ -3,15 +3,17 @@
 # Kill all three terminals for nuc_control since kill-session does not have sudo access.
 
 # Set up tmux
+session=$1
 
 unset -f _tmux_send_keys_all_panes_
+
 _tmux_send_keys_all_panes_() {
-  for _pane in $(tmux list-panes -s -t 'nuc_control' -F '#P'); do
+  for _pane in $(tmux list-panes -s -t "$session" -F '#P'); do
     tmux send-keys -t ${_pane} C-c C-m
   done
 }
 
-session="nuc_control"
+
 
 
 _tmux_send_keys_all_panes_ 
