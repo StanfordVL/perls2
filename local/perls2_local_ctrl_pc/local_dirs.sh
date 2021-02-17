33b3567170eeb4c074d51062c10f77f5ca492d8f
Danfei Xu <morphine1021@gmail.com>
2021-02-17 15:24:10 -0800

    Clarify instr for local bash
    Clarify that variables for redis conf and passfile should not be changed unless using files located in different directory from perls2_local_control_pc

--- local/perls2_local_ctrl_pc/local_dirs.sh
+++ local/perls2_local_ctrl_pc/local_dirs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Source virtualenv environment with perls2 installed
+# Command to source virtualenv environment with perls2 installed
 export SOURCE_ENV_CMD="source ~/p2env/bin/activate"
 # Directory for perls2 repo
 export PERLS2_DIR="$HOME/perls2"
 # Directory for franka-panda-iprl repo
 export DRIVER_DIR="$HOME/franka-panda-iprl"
+########################################################
+# Only edit below if you want to use a different redis conf or store your password in a directory outside perls2_local_control_pc
 # Path to redis conf file.
 export REDIS_CONF="redis.conf"
 # Path to redis pass file
-export REDIS_PASS="redis_passfile.txt"
+export REDIS_PASS="redis_passfile.txt"  
